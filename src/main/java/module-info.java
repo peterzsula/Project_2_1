9ca2d22f27d6b0ddd3a3653424fcebbcdef37dc9
Zsula, Péter (Stud. DACS) <p.zsula@student.maastrichtuniversity.nl>
2024-10-07 12:23:30 +0200

    copying project to better project structure

---
+++ src/main/java/module-info.java
@@ -0,0 +1,13 @@
+module org.testing.project_2_1 {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires net.synedra.validatorfx;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens org.testing.project_2_1 to javafx.fxml;
+    exports org.testing.project_2_1;
+}
---
+++ src/main/java/org/testing/project_2_1/Piece.java
@@ -0,0 +1,2 @@
+package org.testing.project_2_1;public class Piece {
+}
